af43f2c0cd979666dadd8b66c4daca313fef7e9d
EngAmal27 <engamalahmed021@gmail.com>
2024-08-14 07:25:19 +0300

    fix the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 import HomePage from './Component/HomePage'
+// import { Signup } from './Component/Signup'
 
 function App() {
   return <HomePage />
-=======
-import HomePage from "./Component/HomePage";
-import { Signup } from "./Component/Signup";
-
-function App() {
-    return <HomePage />;
-    // return <Signup />;
->>>>>>> aa4ec6a5537e3183283292bb290c79aba6be9e9f
+  // return <Signup />;
 }
 
-export default App;
+export default App
--- src/Component/Footer.tsx
+++ src/Component/Footer.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Box, Link, Typography } from '@mui/material'
 
 // the footer link
@@ -6,19 +5,11 @@ const Footer = () => {
   return (
     <Box
       sx={{
-        // position: 'relative',
-        zIndex: 1,
-        textDecoration: 'underline',
-        // width: '100%',
-        height: 'auto',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
+        // width: "214px",
+        height: '19px',
         position: 'absolute',
-        bottom: '34px',
-        left: '200px',
-        gap: '0px',
-        opacity: 1
+        bottom: '40px',
+        left: '200px'
       }}
     >
       <Typography
@@ -26,14 +17,14 @@ const Footer = () => {
         sx={{
           fontFamily: 'Inter, sans-serif',
           fontWeight: 400,
+          color: '#fff',
           fontSize: '16px',
           lineHeight: '19.36px',
-          letterSpacing: '0.02em',
-          color: '#FFFFFF'
+          letterSpacing: '0.02em'
         }}
       >
         View this project on{' '}
-        <Link href='https://github.com' sx={{ color: '#e786ff' }} underline='always'>
+        <Link href='https://github.com' sx={{ color: 'rgba(231, 134, 255, 1)' }} underline='always'>
           Github
         </Link>
       </Typography>
@@ -42,44 +33,3 @@ const Footer = () => {
 }
 
 export default Footer
-=======
-import { Box, Link, Typography } from "@mui/material";
-
-// the footer link
-const Footer = () => {
-    return (
-        <Box
-            sx={{
-                // width: "214px",
-                height: "19px",
-                position: "absolute",
-                bottom: "40px",
-                left: "200px",
-            }}
-        >
-            <Typography
-                variant="body2"
-                sx={{
-                    fontFamily: "Inter, sans-serif",
-                    fontWeight: 400,
-                    color: "#fff",
-                    fontSize: "16px",
-                    lineHeight: "19.36px",
-                    letterSpacing: "0.02em",
-                }}
-            >
-                View this project on{" "}
-                <Link
-                    href="https://github.com"
-                    sx={{ color: "rgba(231, 134, 255, 1)" }}
-                    underline="always"
-                >
-                    Github
-                </Link>
-            </Typography>
-        </Box>
-    );
-};
-
-export default Footer;
->>>>>>> aa4ec6a5537e3183283292bb290c79aba6be9e9f
