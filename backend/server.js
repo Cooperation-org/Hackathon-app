65ca669ed1288c89b1acce680c64c33652d20bf3
Anuja Chivate <anuja.chivate67@gmail.com>
2024-08-20 17:44:22 -0700

    fixing the project setup

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,11 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const hederaRoutes = require('./routes/hederaRoutes');
-const electionRoutes = require('./routes/electionRoutes');
-const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./config/swagger');
-
-
-require('dotenv').config();
+const express = require("express");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const hederaRoutes = require("./routes/hederaRoutes");
+const electionRoutes = require("./routes/electionRoutes");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./config/swagger");
+const { PORT } = require("./environment");
 
 const app = express();
 
@@ -18,11 +16,14 @@ connectDB();
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/metamask', hederaRoutes);
-app.use('/api/election', electionRoutes);
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api/auth", authRoutes);
+app.use("/api/metamask", hederaRoutes);
+app.use("/api/election", electionRoutes);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT} and swagger on /docs route`));
+const SERVER_PORT = PORT || 5000;
+app.listen(SERVER_PORT, () =>
+    console.log(
+        `Server running on port ${SERVER_PORT} and swagger on /docs route`
+    )
+);
